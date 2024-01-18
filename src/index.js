e2b01c929218a30acf05e73eacdd02ef60522f97
Karl <124084710+ltek0@users.noreply.github.com>
2024-01-18 13:58:20 +0000

    customization

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import './prog24.css';
 
-import Prog24 from './comp/prog24';
+const Prog24 = () => {
+    const e = new Date(Date.UTC(2024, 0, 1));
+    const [t, setT] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setT(new Date());
+        }, 1000);
+        return () => clearInterval(timer);
+    }, []);
+
+    let d = (Math.abs(e - t)/1e3/60/60/24/365)*100;
+
+    return (
+        <div className='progress2024'>  
+        <div>
+            <table>
+                <tr>
+                    <td><p>Today is</p></td>
+                    <td><p>&nbsp;<span style={{color: '#ffa500'}}>{t.toDateString()}</span></p></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td><p>Time</p></td>
+                    <td><p>&nbsp;<span style={{color: '#ffa500'}}>{t.getHours()}:{t.getMinutes()}:{t.getSeconds()}</span></p></td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <td colSpan={2}>
+                        <div className="bar"><div style={{width: d+'%'}}></div></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td colSpan={2}><p>2024 is <span style={{color: '#ffa500'}}>{d.toFixed(3)}%</span> complete</p></td>
+                </tr>
+            </table>
+        </div>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
